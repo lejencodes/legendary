32b04c3d46563dd04a91f1a577320675c9bf31b1
lejencodes <lejencodes@gmail.com>
2022-11-26 13:43:43 +0800

    fix firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,7 +6,7 @@ firebase.initializeApp({
   apiKey: process.env.API_KEY,
   authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
   projectId: process.env.PROJECT_ID,
-  storageBucket: `${process.env.ROJECT_ID}.appspot.com`,
+  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.SENDER_ID,
   appId: process.env.APP_ID,
 });
